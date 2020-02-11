42d51f73ce2780d5c601dc9f6bb2de49f9559f23
Aldo André <aldoandre.jc@gmail.com>
2020-02-11 16:23:58 -0300

    First Commit Portafolio

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  $('a[href^="#"]').click(function() {
+    var destino = $(this.hash);
+    var diferencial = 0;
+    if (destino.length == 0) {
+      destino = $('a[name="' + this.hash.substr(1) + '"]');
+    }
+    if (destino.length == 0) {
+      destino = $('html');
+    }
+    if (this.hash == "#descripcion") {
+      diferencial = 100;
+    }
+
+    if (this.hash == "#experiencia") {
+      diferencial = 100;
+    }
+
+    if (this.hash == "#trabajos") {
+      diferencial = 60;
+    }
+    if (this.hash == "#contact-form") {
+      diferencial = 13;
+    }
+    $('html, body').animate({
+      scrollTop: destino.offset().top - diferencial
+    }, 500);
+    return false;
+  });
+  $('[data-toggle="tooltip"]').tooltip();
+  $(".close").click(function() {
+    $("#myAlert").alert("close");
+  });
+  $('#carousel-Slidesonly').carousel({
+    interval: 5000
+  });
+});
